5b2882c571d892b952328e2f18aba9282b4e132c
Michael Topp <blog@codeschubser.de>
2016-01-06 10:40:46 +0100

    add check_requirements() function

--- includes/functions.php
+++ includes/functions.php
@@ -30,3 +30,29 @@
  * @license     http://www.opensource.org/licenses/mit-license.html  MIT License
  * @version     $Id: functions.php,v 0.0.1 06.01.2016 10:07:06 mitopp Exp $;
  */
+/**
+ * Check for the required PHP version, and the MySQL extension.
+ * Dies if requirements are not met.
+ *
+ * @since   0.0.1
+ *
+ * @access  private
+ * @return  void
+ */
+function check_requirements()
+{
+    $php_version = phpversion();
+
+    if ( version_compare( REQUIRED_PHP_VERSION, $php_version, '>' ) ) {
+        header( sprintf( '%s 500 Internal Server Error', $_SERVER['SERVER_PROTOCOL'] ), true, 500 );
+        header( 'Content-Type: text/html; charset=utf-8' );
+        die( sprintf( 'Your server is running PHP version %1$s but Honeycomb %2$s requires at least %3$s.',
+                $php_version, '0.0.1', REQUIRED_PHP_VERSION ) );
+    }
+
+    if ( ! extension_loaded( 'mysql' ) && ! extension_loaded( 'mysqli' ) && ! extension_loaded( 'pdo' ) ) {
+        header( sprintf( '%s 500 Internal Server Error', $_SERVER['SERVER_PROTOCOL'] ), true, 500 );
+        header( 'Content-Type: text/html; charset=utf-8' );
+        die( 'Your PHP installation appears to be missing the MySQL PDO extension wich is required by Honeycomb.' );
+    }
+}
